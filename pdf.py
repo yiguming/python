7377d6f32df38e2ae00ab3661de0c7893c031418
yiguming <439309415@qq.com>
2014-06-13 10:39:36 +0800

    create a pdf file
    create a pdf file

---
+++ pdf.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#-*-coding:utf8-*-
+from reportlab.graphics.shapes import Drawing,String
+from reportlab.graphics import renderPDF
+d = Drawing(100,100)
+s = String(50,50,'guming is amazing!',textAnchor = 'middle')
+d.add(s)
+renderPDF.drawToFile(d,'hello.pdf','A simple PDF file')
